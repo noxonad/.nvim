541303546255ebfd228924a66504e8ccb3b5df73
noxonad <noxonad@proton.me>
2024-09-11 02:54:52 +0300

    linux number and welcome screen fix
    - Fixed pika on the main screen;
     - Added both relative and absolute line number.

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -7,24 +7,24 @@ return {
 
     -- set header
     dashboard.section.header.val = {
-      [[          ▀████▀▄▄              ▄█ ]],
-      [[            █▀    ▀▀▄▄▄▄▄    ▄▄▀▀█ ]],
-      [[    ▄        █          ▀▀▀▀▄  ▄▀  ]],
+      [[          ▀█▀▀▀▀▀▄▄              ▄█ ]],
+      [[            █     ▀▀▄▄▄▄▄    ▄▄▀▀█ ]],
+      [[    █▄       █          ▀▀▀▀█  ▄▀  ]],
       [[   ▄▀ ▀▄      ▀▄              ▀▄▀  ]],
-      [[  ▄▀    █     █▀   ▄█▀▄      ▄█    ]],
-      [[  ▀▄     ▀▄  █     ▀██▀     ██▄█   ]],
+      [[  ▄▀    █     █▀   ▄█▀▄      ▄█▄   ]],
+      [[  ▀▄     ▀▄  █     ▀██▀     ██▄██  ]],
       [[   ▀▄    ▄▀ █   ▄██▄   ▄  ▄  ▀▀ █  ]],
       [[    █  ▄▀  █    ▀██▀    ▀▀ ▀▀  ▄▀  ]],
       [[   █   █  █                   ▄▀   ]],
     }
 
     dashboard.section.buttons.val = {
-      dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
-      dashboard.button("f", "  > Find file", ":cd $HOME/Workspace | Telescope find_files<CR>"),
+      dashboard.button("e",  "  > New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("f",  "  > Find file", ":cd $HOME/Workspace | Telescope find_files<CR>"),
       dashboard.button("wr", "󰁯  > Restore session", "<cmd>SessionRestore<CR>"),
-      dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
-      dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-      dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
+      dashboard.button("r",  "  > Recent", ":Telescope oldfiles<CR>"),
+      dashboard.button("s",  "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+      dashboard.button("q",  "  > Quit NVIM", ":qa<CR>"),
     }
 
     -- apply the confid
