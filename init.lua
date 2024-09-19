f5fe95ff542de03d3ad117a240510182182d40b8
noxonad <noxonad@proton.me>
2024-09-19 15:00:53 +0300

    notification fix
    fixed most of the annoying notifications

--- init.lua
+++ init.lua
@@ -1,2 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.diagnostic.config({
+  virtual_text = {
+    severity = {
+      min = vim.diagnostic.severity.ERROR, -- Adjust this to your needs
+    },
+  },
+})
