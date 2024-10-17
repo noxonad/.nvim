662166893cb752d27f584b711c6d407e1f47e86a
noxonad <noxonad@proton.me>
2024-10-17 17:42:57 +0300

    surround change

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,6 +1,7 @@
 return {
   "kylechui/nvim-surround",
-  event = { "BufReadPre", "BufNewFile" },
   version = "*",
-  config = true,
+  config = function()
+    require("nvim-surround").setup({})
+  end,
 }
